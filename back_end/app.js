4126b4b55f14d24b21973e86924b67348e3af91a
Mohamed Saeed <moesaeed2014.ms@gmail.com>
2021-05-06 17:59:27 -0500

    Added initial setup to api call

--- back_end/app.js
+++ back_end/app.js
@@ -1,9 +1,22 @@
 const express = require('express');
 const app = express();
+const axios = require('axios');
 
+let call = () => {
+    axios({
+        method:'GET',
+        url: 'https://wger.de/api/v2/exercise/?muscles=1',
+        headers:{
+            Accept: 'application/json',
+            Authorization: `Token `
+        }
+    }).then(response => {
+        console.log(response.data);
+    });
+}
 
 app.get('/', (req, res) => {
-    console.log("hello world");
+    call();
 });
 
 module.exports = app;
