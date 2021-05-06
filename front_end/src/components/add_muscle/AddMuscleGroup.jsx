7514231ac01e3ffddfd17bdebb8fdca5c0bd10d3
Mohamed Saeed <moesaeed2014.ms@gmail.com>
2021-05-06 18:00:11 -0500

    Fixed button onclick event

--- front_end/src/components/add_muscle/AddMuscleGroup.jsx
+++ front_end/src/components/add_muscle/AddMuscleGroup.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import './addmusclegroup.css';
-import {PlusCircleIcon} from '@primer/octicons-react';
 import axios from 'axios';
 
 class AddMuscleGroup extends React.Component{
 
     addMuscleToList = () =>{
+        console.log("Here")
         this.props.addMuscleToList();
         axios.get(`http://www.localhost:3001/`)
         .then(() => {
@@ -16,7 +16,11 @@ class AddMuscleGroup extends React.Component{
     render(){
         return(
             <div className="add">
-                <PlusCircleIcon onClick={() => {this.addMuscleToList()}} size={30} className="addButton" />
+                <button className="addButton" onClick={this.addMuscleToList}>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" className="bi bi-plus" viewBox="0 0 16 16">
+                        <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
+                    </svg>                
+                </button>
             </div>
         )
     }
